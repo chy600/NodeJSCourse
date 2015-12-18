10049b0a3e1cc4a595cc1f56e2066ddc3d290940
Shu Yu <shu@Omnilogics-Mac-mini.local>
2015-12-18 16:45:35 -0600

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('style', function () {
+    gulp.src(jsFiles)
+        .pipe(jshint());
+});
