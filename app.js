9bda9bd2c4ee93c9debca716e19282ffcf7d3d55
Shu Yu <shu@Omnilogics-Mac-mini.local>
2015-12-21 17:18:57 -0600

    *Updated to MongoDB section.

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
+var nav = [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+var adminRouter = require('./src/routes/adminRoutes')(nav);
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+
+app.set('view engine', 'ejs');
+
+app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
 
 //Routing
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.render('index', {
+        title: 'Hello from render',
+        nav: nav
+    });
 });
 
 app.get('/books', function (req, res) {
