df758869cdcca7ba4f649ed7b333756fcfed49ac
Shu Yu <shu@Omnilogics-Mac-mini.local>
2015-12-22 11:09:00 -0600

    * Updated to Authentication module

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
 var app = express();
+
 var port = process.env.PORT || 5000;
 var nav = [{
             Link: '/Books',
@@ -10,14 +15,24 @@ var nav = [{
         }];
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+require('./src/config/passport')(app);
+
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 //Routing
 app.get('/', function (req, res) {
